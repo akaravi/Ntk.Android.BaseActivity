4be47616425315f2ce8e976a7f4f5e90dd28f503
ixiflutter1313 <mj.parishani@outlook.com>
2020-11-30 17:35:56 +0330

    fix resourse

--- myapplication/src/main/java/ntk/android/myapplication/MainActivity.java
+++ myapplication/src/main/java/ntk/android/myapplication/MainActivity.java
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ntk.android.base.activity.common.BaseSplashActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends BaseSplashActivity {
+
+
 }

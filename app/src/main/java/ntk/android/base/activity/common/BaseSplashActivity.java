e38505b935004a02542d9fec8b9ba3ac5ffdfa99
ixiflutter1313 <mj.parishani@outlook.com>
2021-06-30 00:23:59 +0430

    add

--- app/src/main/java/ntk/android/base/activity/common/BaseSplashActivity.java
+++ app/src/main/java/ntk/android/base/activity/common/BaseSplashActivity.java
@@ -10,6 +10,7 @@ import ntk.android.base.NTKApplication;
 import ntk.android.base.R;
 import ntk.android.base.activity.abstraction.AbstractSplashActivity;
 import ntk.android.base.utill.FontManager;
+import ntk.android.base.view.ThemeNameEnum;
 
 public abstract class BaseSplashActivity extends AbstractSplashActivity {
 
@@ -17,18 +18,44 @@ public abstract class BaseSplashActivity extends AbstractSplashActivity {
     @Override
     protected void onCreated() {
         SplashViewController splashViewController;
-        NTKApplication.getApplicationStyle().getTheme();
-        splashViewController = SplashViewController();
+        //create splash base on theme
+        if (NTKApplication.getApplicationStyle().getTheme() == ThemeNameEnum.THEME1)
+            splashViewController = SplashViewController1();
+        else
+            splashViewController = SplashViewController2();
+
         setContentView(splashViewController.getLayout());
         splashViewController.buildUi();
     }
 
 
-    public SplashViewController SplashViewController() {
+    public SplashViewController SplashViewController1() {
         return new SplashViewController() {
             @Override
             public int getLayout() {
                 return R.layout.common_splash_activity;
+            }
+
+            @Override
+            public void buildUi() {
+                TextView Lb2 = findViewById(R.id.lblWorkActSplash);
+                TextView Lbl = findViewById(R.id.lblVersionActSplash);
+                findViewById(R.id.splash_debugView).setOnClickListener(BaseSplashActivity.this::showDebugView);
+                findViewById(R.id.activity_BaseError).findViewById(R.id.debugModeView).setOnClickListener(BaseSplashActivity.this::showDebugView);
+                if (Lb2 != null)
+                    Lb2.setTypeface(FontManager.T1_Typeface(BaseSplashActivity.this));
+                Lbl.setTypeface(FontManager.T1_Typeface(BaseSplashActivity.this));
+                Lbl.setText(getString(R.string.version_number) + "  " + (BaseNtkApplication.get().getApplicationParameter().VERSION_NAME())
+                        + "    " + getString(R.string.build_number) + "  " + BaseNtkApplication.get().getApplicationParameter().VERSION_CODE());
+            }
+        };
+    }
+
+    public SplashViewController SplashViewController2() {
+        return new SplashViewController() {
+            @Override
+            public int getLayout() {
+                return R.layout.common_splash_activity_2;
             }
 
             @Override

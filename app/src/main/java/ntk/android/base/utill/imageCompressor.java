25a042be1acb9bef0d3138822288181b3100ed98
ixiflutter1313 <mj.parishani@outlook.com>
2022-06-18 22:09:06 +0430

    Create imageCompressor.java

---
+++ app/src/main/java/ntk/android/base/utill/imageCompressor.java
@@ -0,0 +1,18 @@
+package ntk.android.base.utill;
+
+class imageCompressor {
+    public static String stringInsert( String  bag,  int index,String marble) {
+        String bagBegin = bag.substring(0,index);
+        String bagEnd = bag.substring(index);
+        return bagBegin + marble + bagEnd;
+    }
+    public  static String convertSizeThumbnailImage(String imageUrl,  int width,int height)
+    {
+        if(imageUrl.toLowerCase().indexOf(".png")>0||imageUrl.toLowerCase().indexOf(".jpg")>0||imageUrl.toLowerCase().indexOf(".jpeg")>0) {
+            String retOut = stringInsert(imageUrl, imageUrl.lastIndexOf("."), "."+width + "x" + height);
+            retOut=retOut.replace("/images/","/thumbnails/");
+            return retOut;
+        }
+        return  imageUrl;
+    }
+}
